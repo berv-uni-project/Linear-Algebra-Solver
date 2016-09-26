2ad27b16b7a1edc4c48f598783fa2d6822588cf0
Bervianto Leo Pratama <bervianto.leo@gmail.com>
2016-09-26 20:12:38 +0700

    add source, last update

---
+++ src/Point.java
@@ -0,0 +1,26 @@
+/*
+	ADT Point
+*/
+
+import java.util.*;
+import java.io.*;
+
+public class Point
+{
+	public double x,y;
+
+	// Konstruktor
+	Point(double x, double y)
+	{
+		this.x = x;
+		this.y = y;
+	}
+
+	// Selector
+	public double GetX() { return this.x; }
+	public double GetY() { return this.y; }
+	// Setter
+	public void SetX(double newX) { this.x = newX; }
+	public void SetY(double newY) { this.y = newY; }
+	
+}
