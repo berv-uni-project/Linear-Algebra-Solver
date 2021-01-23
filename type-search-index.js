dd8b4cc218b2bda70626c8cc091c255e89ece317
berviantoleo <berviantoleo@users.noreply.github.com>
2021-01-23 08:56:14 +0000

    deploy: 0bdf853374996a148979bbdd2eee96d2ff160754

--- type-search-index.js
+++
@@ -1,1 +0,0 @@
-typeSearchIndex = [{"l":"All Classes","url":"allclasses-index.html"},{"p":"linear.algebra.solver","l":"Interface"},{"p":"linear.algebra.solver","l":"Matrix"},{"p":"linear.algebra.solver","l":"Menu"},{"p":"linear.algebra.solver","l":"Point"}]
